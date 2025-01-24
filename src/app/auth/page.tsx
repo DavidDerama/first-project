c3de523e04f3a620f6c1ffb6c7f155d558949b3d
DavidDerama <davidbrentderama@gmail.com>
2025-01-24 17:25:58 +0200

    improved auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -18,28 +18,31 @@ export default function page() {
   }
   return (
     <main className="flex w-screen h-screen justify-center items-center bg-white dark:bg-dark">
-      <section className="w-[460px] flex flex-col px-2">
+      <section className="w-[460px] flex flex-col px-2 relative">
+        <div className="flex justify-center items-center absolute -top-14 left-1/2 -translate-x-1/2">
+          <Image alt="logo" height={40} width={38} src="/icon.png" />
+        </div>
         <h2 className="text-4xl font-bold text-center">Sign In</h2>
         <Button
-          className="w-4/5 mx-auto mt-10 mb-7 rounded-lg text-xl font-bold py-6 flex gap-3 items-center border-dark_input_border  bg-black text-light  hover:bg-black hover:text-light border border-black hover:opacity-85"
+          className="w-4/5 mx-auto mt-8 mb-2 rounded-lg text-lg font-bold py-6 flex gap-3 items-center border-dark_input_border  bg-black text-light  hover:bg-black hover:text-light border border-black hover:opacity-85"
           onClick={handleLoginWithGoogle}
         >
           <FcGoogle />
           Continue with google
         </Button>
-        <div className="flex gap-2 items-center mx-auto">
-          <Image alt="logo" height={40} width={24} src="/icon.png" />
-          <h2 className="text-base text-dark dark:text-light font-bold">
-            LateControl
-          </h2>
-        </div>
         <p className="text-center text-sm mt-5 w-4/5 mx-auto text-light_label dark:text-dark_label">
-          By clicking continue, you agree to our{" "}
-          <Link href="../" className="hover:underline">
+          By clicking continue, you agree to our <br />
+          <Link
+            href="../"
+            className="underline hover:text-light_paragraph dark:hover:text-dark_paragraph"
+          >
             Terms of Service
           </Link>{" "}
           and{" "}
-          <Link href="../" className="hover:underline">
+          <Link
+            href="../"
+            className="underline hover:text-light_paragraph dark:hover:text-dark_paragraph"
+          >
             Privacy Policy
           </Link>
           .
