b4daf17224902597e014e3252bd77f58ec38a3da
DavidDerama <davidbrentderama@gmail.com>
2025-01-28 18:56:35 +0200

    added some text to faq

--- src/components/faq/AccordionFaq.tsx
+++ src/components/faq/AccordionFaq.tsx
@@ -8,20 +8,49 @@ import {
 const items = [
   {
     title: "What does the app do exactly?",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.s",
+    content: (
+      <p>
+        LateControl helps you break the cycle of staying up late and waking up
+        in a hurry or not getting anything done the next day by blocking
+        distracting websites at bedtime and encouraging you to achieve daily
+        tasks that gets you closer to your future goals.
+        <br />
+        <br />
+        The app also ensures you stay productive and accountable with features
+        like AI recommended tasks based on your goals, future goals tracking,
+        and a supportive discord community.
+      </p>
+    ),
   },
   {
     title: "Who is this for?",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.s",
+    content: (
+      <p>
+        This app is for you if you're a workaholic, a student, a parent wanting
+        to control your kid's PC usage, or just someone who stays up late
+        browsing on their computer.
+      </p>
+    ),
   },
   {
     title: "How will this help me?",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.s",
+    content: (
+      <p>
+        By sleeping early and getting your goals done, you’re not just getting
+        enough rest, you’re setting yourself up for long-term success.
+        <br />
+        <br />
+        LateControl simplifies habit tracking, blocks distractions, and helps
+        you manage your time effectively by showing future goals and the tasks
+        related to achieving them. No complex systems or steep learning curves
+        just a clear, simple way to achieve your dream self.
+      </p>
+    ),
   },
   {
     title:
       "What's the difference between the subscription and the lifetime access?",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.s",
+    content: <p></p>,
   },
 ];
 
@@ -34,7 +63,7 @@ export function AccordionFaq() {
             <AccordionTrigger className="text-xl font-semibold">
               {item.title}
             </AccordionTrigger>
-            <AccordionContent className="text-lg">
+            <AccordionContent className="text-lg text-light_paragraph dark:text-dark_paragraph">
               {item.content}
             </AccordionContent>
           </AccordionItem>
