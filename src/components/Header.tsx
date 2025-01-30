12861ab45e3ed7cda506e3ea809e89637e46547b
DavidDerama <davidbrentderama@gmail.com>
2025-01-30 15:36:55 +0200

    header restyled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,31 +7,29 @@ import { FaChrome } from "react-icons/fa";
 export default function Header() {
   return (
     <header className="w-full">
-      <Content className="flex xl:flex-row flex-col gap-8 lg:justify-between pt-48 pb-12">
-        <div className="max-w-[650px] flex flex-col justify-center mx-auto xl:m-0">
-          <h1 className="text-6xl text-light_heading dark:text-dark_heading font-black leading-none -tracking-[0.4px] text-center xl:text-left mb-5">
+      <Content className="flex flex-col gap-8 pt-40 pb-12">
+        <div className="max-w-[750px] flex flex-col justify-center mx-auto">
+          <h1 className="text-6xl text-light_heading dark:text-dark_heading font-black leading-none -tracking-[0.4px] text-center w-full mb-5">
             <Balancer>
               <span className="text-accent_custom">Get More Done</span> Tomorrow
               by Sleeping on Time<br></br>{" "}
             </Balancer>
           </h1>
-          <p className="text-xl font-light text-light_paragraph dark:text-dark_paragraph leading-relaxed xl:text-left text-center mb-10">
-            <Balancer>
-              LateControl blocks distractions at bedtime and ensures you wake up
-              energized, knowing you had a productive day.
-            </Balancer>
+          <p className="text-xl font-light text-light_paragraph dark:text-dark_paragraph leading-relaxed text-center mb-10">
+            LateControl blocks distractions at bedtime and ensures you wake up
+            energized, knowing you had a productive day.
           </p>
-          <button className="bg-accent_custom text-white self-start py-3 px-10 text-base lg:text-xl rounded-full hover:opacity-80 mx-auto xl:m-0">
+          <button className="bg-accent_custom text-white self-start py-3 px-10 text-base lg:text-xl rounded-full hover:opacity-80 mx-auto">
             Get LateControl
           </button>
-          <div className="mt-6 flex gap-3 text-light_label items-center text-lg mx-auto xl:mx-0 bg-transparent">
+          <div className="mt-6 flex gap-3 text-light_label items-center text-lg mx-auto bg-transparent">
             <p>Available on: </p>
             <div className="flex gap-3">
               <FaChrome />
             </div>
           </div>
         </div>
-        <div className="max-w-[380px] lg:max-w-[600px] rounded border-2 shadow-md border-dark_input_border overflow-hidden flex justify-center items-center mx-auto xl:m-0 mt-6">
+        <div className="max-w-[380px] lg:max-w-[600px] rounded border-2 shadow-md border-dark_input_border overflow-hidden flex justify-center items-center mx-auto mt-6">
           <Image
             width={500}
             height={400}
